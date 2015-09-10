aa833835be080d67171516e189d354a764f1c6a6
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-09-09 17:23:12 -0700

    Added schemas for all API calls...

---
+++ schemas/constants.js
@@ -0,0 +1,18 @@
+/**
+ * This module defines a mapping from keys to value that can be rendered into
+ * JSON schemas using {$const: "my-key"}
+ *
+ * This enables us to reuse the same slugid-pattern everywhere we define slugids
+ * but give a different description of each occurrence. It makes it easy to
+ * maintain consistent schemas without using `$ref`s for every single pattern
+ * that can be reused.
+ */
+module.exports = {
+  // Things like clientId
+  "identifier-pattern":       "^[a-zA-Z0-9-_]{1,22}$",
+  "access-token-pattern":     "^[a-zA-Z0-9-_]{22,66}$",
+
+  // Slugid pattern, for when-ever that is useful
+  "slugid-pattern":           "^[A-Za-z0-9_-]{8}[Q-T][A-Za-z0-9_-]" +
+                              "[CGKOSWaeimquy26-][A-Za-z0-9_-]{10}[AQgw]$",
+};
