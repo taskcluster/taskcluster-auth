c8ac62868c85192a850d164cd847ec32dd13fa5f
owlishDeveloper <adamanna.ca@gmail.com>
2016-10-12 16:53:39 -0700

    Changed ping() description

--- src/v1.js
+++ src/v1.js
@@ -880,9 +880,8 @@ api.declare({
   stability:  'experimental',
   title:    "Ping Server",
   description: [
-    "Documented later...",
-    "",
-    "**Warning** this api end-point is **not stable**."
+    "Respond without doing anything.",
+    "This endpoint is used to check that the service is up."
   ].join('\n')
 }, function(req, res) {
   res.status(200).json({
